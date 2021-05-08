6135ac06826760dfd6639813a094b4835d6a7f97
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-05-08 16:01:53 -0300

    Error deploy Dashboard IMG never used fix

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 
-import ImgDsDark from 'assets/img/ds-dark.svg';
 import BarChart from 'components/BarChart';
 import DataTable from 'components/datatable';
 import DonutChart from 'components/DonutChart/Index';
